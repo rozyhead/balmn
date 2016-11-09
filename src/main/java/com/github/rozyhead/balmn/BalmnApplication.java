b49dea1144a0c8c4260ca4114cadbf1b1183b843
Takeshi Miyajima <rozyhead@gmail.com>
2016-11-10 00:59:28 +0900

    Create by spring initializer

---
+++ src/main/java/com/github/rozyhead/balmn/BalmnApplication.java
@@ -0,0 +1,12 @@
+package com.github.rozyhead.balmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalmnApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/rozyhead/balmn/BalmnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.rozyhead.balmn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BalmnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
