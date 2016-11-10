751fa3b8cfe4ee66c2695b101f0a8692796528e6
Takeshi Miyajima <rozyhead@gmail.com>
2016-11-11 00:53:34 +0900

    Change java to kotlin

---
+++ src/main/kotlin/com/github/rozyhead/balmn/BalmnApplication.kt
@@ -0,0 +1,26 @@
+package com.github.rozyhead.balmn
+
+import org.jetbrains.exposed.spring.SpringTransactionManager
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import javax.sql.DataSource
+
+@SpringBootApplication
+@EnableTransactionManagement
+open class BalmnApplication {
+
+  @Bean
+  open fun transactionManager(dataSource: DataSource) = SpringTransactionManager(dataSource)
+
+  @Bean
+  open fun persistenceExceptionTranslationPostProcessor() = PersistenceExceptionTranslationPostProcessor()
+
+}
+
+fun main(args: Array<String>) {
+  SpringApplication.run(BalmnApplication::class.java, *args)
+}
+
