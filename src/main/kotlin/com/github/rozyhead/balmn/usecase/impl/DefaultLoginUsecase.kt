3d1a3431485700696af19c2977b0cb8375fb8b97
Takeshi Miyajima <rozyhead@gmail.com>
2016-11-16 00:08:02 +0900

    Update usecase

--- src/main/kotlin/com/github/rozyhead/balmn/usecase/impl/DefaultLoginUsecase.kt
+++ src/main/kotlin/com/github/rozyhead/balmn/usecase/impl/DefaultLoginUsecase.kt
@@ -1,19 +1,31 @@
 package com.github.rozyhead.balmn.usecase.impl
 
+import com.github.rozyhead.balmn.domain.model.account.AccountName
 import com.github.rozyhead.balmn.domain.model.account.UserAccount
-import com.github.rozyhead.balmn.domain.model.authentication.PasswordAuthenticationService
+import com.github.rozyhead.balmn.domain.model.account.UserAccountRepository
+import com.github.rozyhead.balmn.domain.model.authentication.PasswordAuthenticationRepository
+import com.github.rozyhead.balmn.domain.model.authentication.PlainPassword
 import com.github.rozyhead.balmn.usecase.LoginFailureException
 import com.github.rozyhead.balmn.usecase.LoginUsecase
 import org.springframework.stereotype.Service
 
 @Service
 open class DefaultLoginUsecase(
-    val passwordAuthenticationService: PasswordAuthenticationService
+    val passwordAuthenticationRepository: PasswordAuthenticationRepository,
+    val userAccountRepository: UserAccountRepository
 ) : LoginUsecase {
 
   override fun execute(command: LoginUsecase.Command): UserAccount {
-    return passwordAuthenticationService.authenticate(
-        command.accountName, command.plainPassword) ?: throw LoginFailureException()
+    val (accountName, plainPassword) = command;
+    return authenticate(accountName, plainPassword) ?: throw LoginFailureException()
+  }
+
+  fun authenticate(accountName: AccountName, plainPassword: PlainPassword): UserAccount? {
+    val passwordAuthentication = passwordAuthenticationRepository.findByAccountName(accountName) ?: return null
+    if (!passwordAuthentication.authenticate(accountName, plainPassword)) {
+      return null;
+    }
+    return userAccountRepository.findByAccountName(accountName)
   }
 
 }
