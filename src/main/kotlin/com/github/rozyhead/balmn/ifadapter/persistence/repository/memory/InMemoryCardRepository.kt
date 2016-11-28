6abb6a49b013480031dffcd712b7873e1648f371
Takeshi Miyajima <rozyhead@gmail.com>
2016-11-29 00:47:44 +0900

    Add InMemoryCardRepository

---
+++ src/main/kotlin/com/github/rozyhead/balmn/ifadapter/persistence/repository/memory/InMemoryCardRepository.kt
@@ -0,0 +1,17 @@
+package com.github.rozyhead.balmn.ifadapter.persistence.repository.memory
+
+import com.github.rozyhead.balmn.domain.model.board.card.CardEvent
+import com.github.rozyhead.balmn.domain.model.board.card.CardIdentifier
+import com.github.rozyhead.balmn.domain.model.board.card.CardRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+open class InMemoryCardRepository : CardRepository {
+
+  val events = mutableMapOf<CardIdentifier, List<CardEvent>>()
+
+  override fun save(cardIdentifier: CardIdentifier, events: List<CardEvent>, oldEvents: List<CardEvent>) {
+    this.events[cardIdentifier] = oldEvents + events
+  }
+
+}
