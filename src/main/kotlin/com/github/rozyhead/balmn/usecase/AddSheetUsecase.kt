cdd7c0038bca55680b59a59f52ce35935f04ee91
Takeshi Miyajima <rozyhead@gmail.com>
2016-11-29 00:06:25 +0900

    Rename usecase

--- src/main/kotlin/com/github/rozyhead/balmn/usecase/AddSheetUsecase.kt
+++ src/main/kotlin/com/github/rozyhead/balmn/usecase/AddSheetUsecase.kt
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-open class AddSheetToBoardUsecase(
+open class AddSheetUsecase(
     val boardRepository: BoardRepository,
     val sheetRepository: SheetRepository
 ) {
