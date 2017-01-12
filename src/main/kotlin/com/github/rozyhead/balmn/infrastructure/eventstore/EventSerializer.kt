2489ebded30f4b5e26fd36457672f159782de6aa
Takeshi Miyajima <rozyhead@gmail.com>
2017-01-13 00:51:19 +0900

    Update

--- src/main/kotlin/com/github/rozyhead/balmn/infrastructure/eventstore/EventSerializer.kt
+++ src/main/kotlin/com/github/rozyhead/balmn/infrastructure/eventstore/EventSerializer.kt
@@ -1,4 +1,11 @@
 package com.github.rozyhead.balmn.infrastructure.eventstore
 
+import com.github.rozyhead.balmn.util.ddd.DomainEvent
+
 interface EventSerializer {
+
+  fun serialize(event: DomainEvent): Array<Byte>
+
+  fun deserialize(type: String, bytes: Array<Byte>): DomainEvent
+
 }
